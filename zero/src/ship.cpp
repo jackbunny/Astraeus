564d4c75d7047f4ac51f204b425daa02d803ec33
Landon Martin <jackdbunny@gmail.com>
2011-07-16 01:32:06 -0500

    Adding ship data and making current

---
+++ zero/src/ship.cpp
@@ -0,0 +1,75 @@
+#include "ship.h"
+#include <iostream> 
+
+Ship::Ship(float x, float y, float acceleration, float maxSpeed,
+           float rotationSpeed) {
+   x_ = x;
+   y_ = y;
+   vx_ = 0;
+   vy_ = 0;
+
+   acceleration_ = acceleration;
+   maxSpeed_ = maxSpeed;
+   rotationSpeed_ = rotationSpeed;
+   thrustIsOn_ = 0;
+   
+   rotation_ = 0;
+   rotationDirection_ = 0;
+}
+
+void Ship::Fire(ISceneManager* sceneManager) {
+   Bullet bullet(x_, y_);
+   sceneManager.Add(bullet);
+}
+
+float Ship::GetRotation() {
+   return rotation_;
+}
+
+float Ship::GetX() {
+   return x_;
+}
+
+float Ship::GetY() {
+   return y_;
+}
+
+void Ship::RotateLeftOn() {
+   rotationDirection_ = 1;
+}
+
+void Ship::RotateRightOn() {
+   rotationDirection_ = -1;
+}
+
+void Ship::RotateOff() {
+   rotationDirection_ = 0;
+}
+
+void Ship::ThrustOn() {
+   thrustIsOn_ = 1;
+}
+
+void Ship::ThrustOff() {
+   thrustIsOn_ = 0;
+}
+
+void Ship::Update(float deltaTime) {
+   float radians = rotation_ * (M_PI / 180.0f);
+   float seconds = deltaTime / 1000.0f;
+
+   if ( thrustIsOn_ ) {
+      vx_ += acceleration_ * cosf(radians) * seconds;
+      vy_ += acceleration_ * sinf(radians) * seconds;
+   }
+
+   x_ += vx_ * seconds; 
+   y_ += vy_ * seconds; 
+
+   rotation_ += rotationSpeed_ * rotationDirection_ * seconds;
+   if ( rotation_ >= 360 ) {
+      rotation_ = rotation_ - 360;
+   } else if (rotation_ < 0) {
+      rotation_ = 360 + rotation_;
+   }
+}
---
+++ zero/src/ship.h
@@ -0,0 +1,36 @@
+#ifndef ZERO_SRC_SHIP_H_ 
+#define ZERO_SRC_SHIP_H_
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
+class Ship {
+public:
+   Ship(float x, float y, float acceleration, float maxSpeed,
+        float rotationSpeed);
+   void Fire(ISceneManager* sceneManager);
+   float GetRotation();
+   float GetX();
+   float GetY();
+   void RotateLeftOn();
+   void RotateRightOn();
+   void RotateOff();
+   void ThrustOn();
+   void ThrustOff();
+   void Update(float deltaTime);
+
+private:
+   float x_;
+   float y_;
+   bool thrustIsOn_;
+   float acceleration_;
+   float maxSpeed_;
+   float rotation_; 
+   float rotationDirection_;
+   float rotationSpeed_;
+   float vx_;
+   float vy_;
+};
+
+#endif //ZERO_SRC_SHIP_H_
